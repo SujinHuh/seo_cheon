0dcade0cd6cfc3cd28c76dd91e300097c186beb2
juin483@naver.com <HgjtnwlsH1!>
2020-10-04 19:18:39 +0900

    DB 컬럼값 변경

--- src/main/java/EHS/MonitoringSystem/contoroller/PopupController.java
+++ src/main/java/EHS/MonitoringSystem/contoroller/PopupController.java
@@ -3,13 +3,18 @@ package EHS.MonitoringSystem.contoroller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PopupController {
 
     @GetMapping("popup/graph")
-    public String graph(Model model) {
-        return "popup/graph";
+    public ModelAndView graph(@RequestParam(value = "sensorId", required = true) String sensorId) {
+        ModelAndView view = new ModelAndView();
+        view.setViewName( "popup/graph");
+        view.addObject("sensorId", sensorId);
+        return view;
     }
 
 }
